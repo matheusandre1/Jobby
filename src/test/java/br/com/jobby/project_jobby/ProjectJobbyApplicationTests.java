3ff6317d63f09f72204f5a7da2c167f4b4a59c98
Matheus Andre <matheusandr2@gmail.com>
2025-09-05 20:09:38 -0300

    fist commit

---
+++ src/test/java/br/com/jobby/project_jobby/ProjectJobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jobby.project_jobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
